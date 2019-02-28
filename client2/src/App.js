06151b75d13cbbd4466f7cacd138c4be0f7dfc75
Tony DeCarlo <anthony.decarlo@t1cg.com>
2019-02-28 13:20:34 -0500

    fix file structure

---
+++ client2/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Fib from './Fib';
+import OtherPage from './OtherPage';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route exact path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
